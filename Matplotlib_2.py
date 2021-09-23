3f0a31428bef887f7e22ab10d8fb078081616570
KingKushal <91248832+KingKushal@users.noreply.github.com>
2021-09-23 20:38:55 +0530

    Minor changes

--- Matplotlib_2.py
+++ Matplotlib_2.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-x1 = [10,20,30,40,50,60,70,80]
-y1 = [72,60,50,45,40,30,20,15]
+x1 = [0,20,30,40,50,60,70,80]
+y1 = [80,60,50,45,40,30,20,0]
 
 plt.plot(x1,y1,label = 'literacy rate')
 
 x2 = x1
-y2 = [10,25,40,55,62,70,75,80]
+y2 = [0,25,40,55,62,70,75,80]
 
 plt.plot(x2,y2,label = 'Financial rate')
 
